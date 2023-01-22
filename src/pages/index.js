9a6008c5817a7822f20ce2a46d2cd5de79a4ee4c
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-01-22 18:43:48 +0530

    Added Favicon + images

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,8 @@ import mihir from "../images/Mihir.jpeg"
 import chaitya from "../images/chaitya.png"
 import vedant from "../images/vedant.jpg"
 import tejas from "../images/tejas.jpeg"
-
+import ratan from "../images/ratan.jpg"
+import favicon from "../images/favicon.ico"
 
 export default (() => {
 	return <Theme theme={theme}>
@@ -21,8 +22,7 @@ export default (() => {
 			<title>
 				AgroDrone - CIIA Exhibition
 			</title>
-			<meta name={"description"} content={"Web site created using quarkly.io"} />
-			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+			<link rel={"shortcut icon"} href={favicon} type={"image/x-icon"} />
 		</Helmet>
 		<Section background="#000000" color="#ffffff">
 			<Box
@@ -1120,7 +1120,7 @@ export default (() => {
 					>
 						<Image
 							border-radius="50%"
-							src="https://images.unsplash.com/photo-1619950466709-02c2bf682442?auto=format&fit=crop&w=300&q=80"
+							src={ratan}
 							object-fit="cover"
 							position="absolute"
 							top={0}
